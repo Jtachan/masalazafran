46e3d1d312142669911dbf84073488c5daa23980
Jtachan <76098522+Jtachan@users.noreply.github.com>
2026-07-31 13:23:37 +0200

    Recipes DB

--- recipes/sweets/index.md
+++ recipes/sweets/index.md
@@ -6,8 +6,8 @@ title: "Sweets"
 
 | Recipe                               | Origin            |
 |:-------------------------------------|:------------------|
-| [Abuela's Apple Cake](applecake.md)  | Spanish :flag_es: |
-| [Abuela's Cheesecake](cheesecake.md) | Spanish :flag_es: |
+| [Abuela's Apple Cake](abuela_apple_cake.md)  | Spanish :flag_es: |
+| [Abuela's Cheesecake](abuela_cheesecake.md) | Spanish :flag_es: |
 | [Flan](flan.md)                      | Spanish :flag_es: |
 | [Frozen Fruit Yoghurt](frozen_yoghurt.md)  | -- |
 | [Ice-Cream (Jam)](ice_cream_jam.md)  | Italian :flag_it: |
---
+++ scan_recipes.py
@@ -0,0 +1,45 @@
+"""Scanning recipes and updating DB.
+
+The update happens only for recipes that do not appear at the current database.
+Some keys are not input (like the nationality). These must be manually updated.
+"""
+
+import json
+from pathlib import Path
+import dataclasses as dtc
+
+ROOT_PATH = Path(__file__).resolve().parent / "recipes"
+DB_PATH = ROOT_PATH / "db.json"
+
+
+@dtc.dataclass
+class Entry:
+    recipe: str  # NAME of the recipe (titled). E.G.: "Pizza", "Focaccia", "Apple Cake".
+    section: str  # SECTION containing the recipe. E.G.: "dough", "sauces", "sweets".
+    nationality: str = (
+        ""  # COUNTRY of the dish, or empty string. E.G.: "Spanish", "Indian", "Italian"
+    )
+
+    def to_dict(self) -> dict:
+        return dtc.asdict(self)
+
+
+if __name__ == "__main__":
+    with open(DB_PATH, "r", encoding="utf-8") as fh:
+        recipes: list[dict] = json.load(fh)
+    all_recipe_names = {r["recipe"] for r in recipes}
+
+    for file in ROOT_PATH.glob("*/*.md"):
+        if (
+            file.name == "index.md"
+            or (name := file.name.split(".")[0].replace("_", " ").title())
+            in all_recipe_names
+        ):
+            continue
+        if name.startswith("Abuela"):
+            name = name.replace("Abuela", "Abuela's")
+        recipes.append(Entry(recipe=name, section=file.parent.name).to_dict())
+
+    recipes.sort(key=lambda r: r["recipe"])
+    with open(DB_PATH, "w", encoding="utf-8") as fh:
+        json.dump(recipes, fh, indent=3)
